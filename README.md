1aa9106d42082f3d2cfc2557ed5d5d246db115ad
Victor Martin <victor.martin.garcia@gmail.com>
2018-09-29 07:31:15 +0200

    Initial commit exercise 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lab exercise 2
+
+More info on Moodle: http://moodle2.udg.edu/course/view.php?id=21067
---
+++ src/main/java/edu/udg/caes/Exercise2.java
@@ -0,0 +1,19 @@
+package edu.udg.caes;
+
+import java.util.Vector;
+
+public class Exercise2 {
+
+    /**
+     * Find objects that are in either of the two argument Vectors
+     *
+     *  @param a first vector
+     *  @param b second vector
+     *  @return objects present in both vectors
+     */
+    public static Vector union (Vector a, Vector b)
+    {
+        throw new UnsupportedOperationException("Non implemented yet");
+    }
+
+}
