c63ce25992faf70de901abc411343ba120702104
Jaume Oliveras Hernández <35967311+u1939653@users.noreply.github.com>
2018-10-08 18:15:27 +0200

    Commit 2: testos de les ambiguetats

--- src/test/java/edu/udg/caes/Exercise2Test.java
+++ src/test/java/edu/udg/caes/Exercise2Test.java
@@ -2,6 +2,8 @@ package edu.udg.caes;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.GregorianCalendar;
+import java.util.InputMismatchException;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,5 +29,55 @@ public class Exercise2Test
         l_vectorResposta.add(4);
 
         Vector l_result = Exercise2.union(l_vectorA,l_vectorB);
-        assertArrayEquals(l_vectorResposta.toArray(),l_result.toArray(),"0");
+        assertEquals(l_vectorResposta,l_result,"");
     }
+
+    //AMBIGUETAT 1: Si un element és null, hauria de retornar null
+    @Test
+    public void unioAmbUnaLlistaANullTest()
+    {
+        Vector l_vectorA = new Vector();
+        l_vectorA.add(1);
+        l_vectorA.add(2);
+
+        Vector l_vectorB = null;
+
+        assertThrows(InputMismatchException.class, () -> Exercise2.union(l_vectorA,l_vectorB));
+    }
+
+    //AMBIGUETAT 2: No s'haurien de permetre repetits
+    @Test
+    public void unioSenseRepetitsTest()
+    {
+        Vector l_vectorA = new Vector();
+        l_vectorA.add(1);
+        l_vectorA.add(2);
+
+        Vector l_vectorB = new Vector();
+        l_vectorB.add(1);
+        l_vectorB.add(2);
+
+        Vector l_vectorResposta = new Vector();
+        l_vectorResposta.add(1);
+        l_vectorResposta.add(2);
+
+        Vector l_result = Exercise2.union(l_vectorA,l_vectorB);
+        assertEquals(l_vectorResposta,l_result,"");
+    }
+
+    //AMBIGUETAT 3: Els vectors han de contenir elements del mateix tipus
+    @Test
+    public void unioDeDiferentsTipusTest()
+    {
+            final Vector l_vectorA = new Vector();
+            l_vectorA.add(1);
+            l_vectorA.add(2);
+
+            final Vector l_vectorB = new Vector();
+            l_vectorB.add(new GregorianCalendar());
+
+            assertThrows(InputMismatchException.class, () -> Exercise2.union(l_vectorA,l_vectorB));
+    }
+
+
+}
